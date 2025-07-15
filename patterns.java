2ed7d28161068ffd2e29d6a788e692d608c34ddb
Aditya Phadatare <adityaphadatare4545@gmail.com>
2025-07-15 16:01:06 +0530

    first commit

---
+++ patterns.java
@@ -0,0 +1,17 @@
+class Main{
+   static void pattern1(int N)
+ {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            System.out.print("* ");       
+        }
+        System.err.println();
+    }
+ }
+
+
+ public static void main(String[] args) {
+     int N = 10;
+     pattern1(N);
+  }
+}
