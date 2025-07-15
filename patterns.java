1678b4e53c2c08385013e72a523a8e036be7e97c
Aditya Phadatare <adityaphadatare4545@gmail.com>
2025-07-15 20:36:48 +0530

    New pattern

--- patterns.java
+++ patterns.java
@@ -9,9 +9,16 @@ class Main{
     }
  }
 
-
+  static void pattern2(int N){
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            System.out.print("- ");
+        }
+        System.err.println("");
+    }
+  }
  public static void main(String[] args) {
-     int N = 10;
-     pattern1(N);
+     int N = 5;
+     pattern2(N);
   }
 }
