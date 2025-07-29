b2cbde5e7dd1124dfc2a461ae1f7cd286451ec0d
Aditya Phadatare <adityaphadatare4545@gmail.com>
2025-07-29 17:09:26 +0530

    Palindrome question done

--- Recursion/Palindrome.java
+++ Recursion/Palindrome.java
@@ -1,5 +1,32 @@
- class Palindrome{
-    static void palindrome(int i, String s){
+class Palindrome{
+   static private boolean isPalindrome(String s){
+      int left =0;
+      int right = s.length()-1;
        
+       while (left<right) { 
+           char l = s.charAt(left), r =s.charAt(right);
+
+           if(!Character.isLetterOrDigit(l))
+             left ++;
+           else if (!Character.isLetterOrDigit(r)) 
+               right --;
+          else if(Character.toLowerCase(l)!= Character.toLowerCase(r))
+               return false;
+            else {
+         left ++;
+         right --;
+        }
+       }
+     return true;
+   }
+    public static void main(String[] args) {
+    String str = "ABCDCBA";
+    boolean ans = isPalindrome(str);
+
+    if (ans == true) {
+      System.out.println("Palindrome");
+    } else {
+      System.out.println("Not Palindrome");
     }
- }
+  }
+}
