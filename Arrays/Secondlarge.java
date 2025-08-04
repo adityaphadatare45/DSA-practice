0fac9c27070d5e37c5f08b6f94d39645f794ef5e
Aditya Phadatare <adityaphadatare4545@gmail.com>
2025-08-04 13:07:04 +0530

    Done with Recursive bubble sort

--- Arrays/Secondlarge.java
+++ Arrays/Secondlarge.java
@@ -1,5 +1,5 @@
  public class Secondlarge{
      public static void main(String[] args){
-     int arr []= {2,4,5,3,1,5,6};
+     // int arr []= {2,4,5,3,1,5,6};
    }
 }
--- Sorting_Techniques/RecursiveBubbleSort.java
+++ Sorting_Techniques/RecursiveBubbleSort.java
@@ -1,3 +1,37 @@
 class RecursiveBubbleSort{
-    
+    static  void bubble_sort(int [] arr, int n){
+        // Base condition for recursion.
+        if(n == 1) return;
+
+        // Recursion loop
+        for(int j = 0; j<= n-2; j++){
+            if(arr[j]> arr[j+1]){
+
+                // swap 
+                int temp = arr[j];
+                arr [j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+        // Range reduced after recursion.
+        bubble_sort(arr, n - 1);
+    }
+    public static void main(String[] args) {
+        int arr[] = {13,46,24,20,9,52};
+
+        int n = arr.length;
+        System.out.println("Before bubble sort");
+
+         for (int i = 0; i < n; i++) {
+             System.out.print(arr[i] + " ");            
+        }
+        System.out.println();
+        bubble_sort(arr, n);
+
+        System.out.println("After bubble sort");
+         for (int i = 0; i < n; i++) {
+            System.out.println( arr [i]+"");             
+        }
+        System.out.println();
+    }
 }
