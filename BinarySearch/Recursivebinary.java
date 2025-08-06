656e92873524ebcad25b9e4fc2ec91335c2e056d
Aditya Phadatare <adityaphadatare4545@gmail.com>
2025-08-06 18:39:30 +0530

    Done recursive binary search

--- BinarySearch/Recursivebinary.java
+++ BinarySearch/Recursivebinary.java
@@ -1,13 +1,28 @@
 class Recursivebinary{
-    static int binary_search( int [] nums , int target){
-         int n = nums.length;
-         int low = 0, high = n -1;
-        return  -1;
+    static int binary_search( int [] nums , int target ,int low, int high){
+        // int n = nums.length;
+         // int low = 0, high = n -1;
+
+         if(low > high){
+            return  -1;
+         }
+
+         int mid = (low + high)/2;
+
+         if(nums[mid] == target){
+            return mid;
+         } else if(target> nums[mid]){
+            return binary_search(nums, target, mid + 1, high);
+         }else{
+          return binary_search(nums, target, low, mid - 1);
+        }
     }
     public static void main(String[] args) {
         int [] nums = { 3, 4, 6, 7, 9, 12, 16, 17};
-        int target = 6;
-        int index = binary_search(nums, target);
+        int target = 16;
+        int low = 0;
+        int high = nums.length - 1;
+        int index = binary_search(nums, target, low, high);
          if(index == -1 ){
           System.out.println(" The target is not present"); 
          }else {
