3d6bd008ea01045e0b0e5335ef23ee161863ef6f
Aditya Phadatare <adityaphadatare4545@gmail.com>
2025-08-08 11:09:47 +0530

    Recursive insertion sort done

--- Sorting_Techniques/RecursiveInsertionSort.java
+++ Sorting_Techniques/RecursiveInsertionSort.java
@@ -1,5 +1,33 @@
 public class RecursiveInsertionSort{
-    static void insertion_sort(int [] arr, int n, int i){
-        
+    static void insertion_sort(int [] nums, int n, int i){
+
+        // Base condition
+       if(i == n) {
+        return;
+       }    
+       int j = i;   
+
+       while(j>0 && nums[j-1] > nums[j]){
+        int temp = nums[j-1];
+        nums [j-1] = nums[j];
+        nums [j] = temp;
+        j--;
+       } 
+       insertion_sort(nums, n, i + 1 );
+    }
+    public static void main(String[] args) {
+        int [] nums = {13, 46, 24, 52, 20, 9};
+        int n = nums.length;
+         System.out.println("Before recursive insertion sort : ");
+          for (int i = 0; i < n; i++) {
+              System.out.println(nums[i] + " ");
+          }
+          System.out.println();
+          insertion_sort(nums, n, 0);
+          System.out.println("After recursive insertion sort : ");
+          for (int i = 0; i < n; i++) {
+              System.out.println(nums[i] + " ");
+          }
+        System.out.println();
     }
 }
