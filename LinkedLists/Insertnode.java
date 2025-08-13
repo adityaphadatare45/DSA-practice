5552bc326a6e9c5cab70e4a01d580c8fbb72e43e
adityaphadatare45 <adityaphadatare4545@gmail.com>
2025-08-13 16:13:09 +0530

    Started linked lists

---
+++ LinkedLists/Insertnode.java
@@ -0,0 +1,17 @@
+package LinkedLists;
+
+import org.w3c.dom.Node;
+
+ class Insertnode {
+	public int data;
+    public Node next;
+
+    public void Node(int data1, Node next1){
+       data = data1;
+       next = next1;
+    }
+
+    public void Node(int data1){
+        
+    }
+}
